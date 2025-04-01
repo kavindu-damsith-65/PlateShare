dde540dcaaf95e5cd27246cd5af6bd5f20b463b4
dulanga <dulangamanusha1@gmail.com>
2025-04-01 17:22:47 +0530

    update

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,6 +1,6 @@
-import { useNavigation, useNavigationState } from "@react-navigation/native";
-import React, { useLayoutEffect, useState, useEffect } from "react";
-import { Image, LogBox, ScrollView, Text, TextInput, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useLayoutEffect } from "react";
+import { Image, ScrollView, Text, TextInput, View , ImageBackground, TouchableOpacity, Dimensions} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import {
@@ -9,40 +9,20 @@ import {
   MagnifyingGlassIcon,
   AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
-import Categories from "../components/Categories";
-import FeaturedRow from "../components/FeaturedRow";
-import SanityClient from "../sanity";
+import Recommended from "../components/Recommended";
+import Restaurants from "../components/Restaurants";
 
 const HomeScreen = () => {
   // state and hooks
   const navigation = useNavigation();
-  const [featuredCategories, setFeaturedCategories] = useState([]);
+
+  const category =  {"_createdAt": "2023-02-17T15:18:29Z", "_id": "f64c7e94-5eb4-49a0-8aeb-f623963ccbca", "_rev": "I13aVDhieoHVSVfjQjpB2w", "_type": "featured", "_updatedAt": "2023-02-17T18:35:01Z", "name": "Satisfy Your Cravings", "restaurants": [[Object], [Object], [Object]], "short_description": "Appetizing, varied, savory, creamy, crunchy"};
 
   // side effects
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
     });
-  }, []);
-
-  useEffect(() => {
-    SanityClient.fetch(
-      `
-      *[_type == "featured"]{
-        ...,
-        restaurants[]->{
-          ...,
-        dishes[]->
-          
-        }
-     }
-    `,
-    )
-      .then((data) => setFeaturedCategories(data))
-      .catch((err) => {
-        console.log("error");
-        console.log(err);
-      });
   }, []);
 
 
@@ -77,18 +57,36 @@ const HomeScreen = () => {
 
         {/* body */}
         <ScrollView className="bg-gray-100" contentContainerStyle={{ paddingBottom: 100 }}>
-          {/* categories */}
-          <Categories />
+
+          {/* Recommended section */}
+          <Recommended />
 
           {/* featured rows */}
-          {featuredCategories.map((category) => (
-            <FeaturedRow
+          <Restaurants
               key={category._id}
               title={category.name}
               description={category.short_description}
               id={category._id}
-            />
-          ))}
+          />
+
+          {/* Give a hand section */}
+          <View style={{ width: Dimensions.get("window").width - 30, height: 200 }} className="bg-white rounded-lg m-4 shadow-lg overflow-hidden">
+            <ImageBackground
+                source={require('./assets/hand.jpg')}
+                style={{ flex: 1, width: "100%" }}
+                resizeMode="cover"
+            >
+              <View className="flex-1 justify-center p-4 bg-black/30">
+                <Text className="text-xl font-black text-white mb-2">Be a Hero, Deliver Hope!</Text>
+                <Text className="text-white mb-4 font-medium">Join our mission to fight food waste and feed those in need.</Text>
+                <TouchableOpacity className="bg-[#00CCBB] rounded-full px-4 py-2 mt-2 self-start">
+                  <Text className="text-white font-bold text-center">Sign Up for Delivery</Text>
+                </TouchableOpacity>
+              </View>
+            </ImageBackground>
+          </View>
+
+
         </ScrollView>
       </SafeAreaView>
     </>
--- backend/models/AuthModel.js
+++ backend/models/AuthModel.js
@@ -20,6 +20,17 @@ const BuyerDetails = sequelize.define('buyer_details', {
     profile_picture: { type: DataTypes.STRING },
 });
 
+// Seller Details Model
+const SellerDetails = sequelize.define('seller_details', {
+    user_id: { type: DataTypes.STRING },
+    name: { type: DataTypes.STRING },
+    email: { type: DataTypes.BOOLEAN },
+    phone: { type: DataTypes.STRING },
+    address: { type: DataTypes.STRING },
+    location: { type: DataTypes.STRING },
+    restaurant_id: { type: DataTypes.STRING },
+}, { primaryKey: ['user_id', 'restaurant_id'] });
+
 // Organization Details Model
 const OrgDetails = sequelize.define('org_details', {
     user_id: { type: DataTypes.STRING, primaryKey: true },
@@ -33,9 +44,102 @@ const OrgDetails = sequelize.define('org_details', {
     additional_images: { type: DataTypes.STRING },
 });
 
+// Product Model
+const Product = sequelize.define('product', {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    name: { type: DataTypes.STRING },
+    type: { type: DataTypes.BOOLEAN, defaultValue: false },
+    available: { type: DataTypes.BOOLEAN, defaultValue: true },
+    quantity: { type: DataTypes.INTEGER },
+    restaurant_id: { type: DataTypes.STRING }
+});
+
+// Restaurant Model
+const Restaurant = sequelize.define('restaurant', {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    name: { type: DataTypes.STRING },
+    user_id: { type: DataTypes.STRING }
+});
+
+// Sub Products Model
+const SubProduct = sequelize.define('sub_products', {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    name: { type: DataTypes.STRING },
+    type: { type: DataTypes.BOOLEAN, defaultValue: false },
+    available: { type: DataTypes.BOOLEAN, defaultValue: true },
+    product_id: { type: DataTypes.STRING },
+    price: { type: DataTypes.DECIMAL(5,2) }
+});
+
+// Food Request Model
+const FoodRequest = sequelize.define('food_request', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    org_details_user_id: { type: DataTypes.STRING },
+    product_id: { type: DataTypes.STRING }
+});
+
+// Admin Model
+const Admin = sequelize.define('admin', {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    role: { type: DataTypes.STRING, defaultValue: 'user' },
+    verified: { type: DataTypes.INTEGER, defaultValue: 0 },
+    email: { type: DataTypes.INTEGER }
+});
+
+// Request Info Model
+const RequestInfo = sequelize.define('request_info', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    food_request_id: { type: DataTypes.INTEGER },
+    product_id: { type: DataTypes.STRING },
+    quantity: { type: DataTypes.INTEGER },
+    completed: { type: DataTypes.BOOLEAN }
+});
+
+// Food Bucket Model
+const FoodBucket = sequelize.define('food_bucket', {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    product_id: { type: DataTypes.STRING },
+    user_id: { type: DataTypes.STRING },
+    sub_products_ids: { type: DataTypes.STRING }
+});
+
+// Payment Model
+const Payment = sequelize.define('payment', {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    order_id: { type: DataTypes.STRING },
+    price: { type: DataTypes.DECIMAL(5,2) }
+});
+
+// Order Model
+const Order = sequelize.define('order', {
+    id: { type: DataTypes.STRING, primaryKey: true },
+    food_bucket_ids: { type: DataTypes.STRING },
+    user_id: { type: DataTypes.STRING },
+    status: { type: DataTypes.INTEGER },
+    price: { type: DataTypes.DECIMAL(5,2) }
+});
+
 // Relationships
 User.hasOne(BuyerDetails, { foreignKey: 'user_id' });
-User.hasOne(OrgDetails, { foreignKey: 'user_id' });
+// User.hasOne(OrgDetails, { foreignKey: 'user_id' });
+// User.hasOne(SellerDetails, { foreignKey: 'user_id' });
+//
+// User.hasMany(Order, { foreignKey: 'user_id' });
+// User.hasMany(FoodBucket, { foreignKey: 'user_id' });
+// User.hasMany(FoodRequest, { foreignKey: 'org_details_user_id' });
+// User.hasMany(Restaurant, { foreignKey: 'user_id' });
+//
+// Restaurant.hasMany(Product, { foreignKey: 'restaurant_id' });
+// Product.hasMany(SubProduct, { foreignKey: 'product_id' });
+// Product.hasMany(RequestInfo, { foreignKey: 'product_id' });
+// Product.hasMany(FoodBucket, { foreignKey: 'product_id' });
+//
+// Order.hasOne(Payment, { foreignKey: 'order_id' });
+// Order.hasMany(FoodBucket, { foreignKey: 'food_bucket_ids' });
+//
+// FoodRequest.hasMany(RequestInfo, { foreignKey: 'food_request_id' });
 
-
-module.exports = { User, BuyerDetails, OrgDetails };
+module.exports = {
+    User, BuyerDetails, SellerDetails, OrgDetails, Product, Restaurant, SubProduct,
+    FoodRequest, Admin, RequestInfo, FoodBucket, Payment, Order
+};
