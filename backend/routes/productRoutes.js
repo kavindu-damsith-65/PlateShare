464e9a24e97b2558848c54f597837641a4ca2799
PrabodhaJayasinghe <110682835+PrabodhaJayasinghe@users.noreply.github.com>
2025-04-26 09:52:14 +0530

    feat: food recommended section

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const {showNearByProducts} = require("../controllers/productController");
+const {showNearByProducts, getRecommendedProducts} = require("../controllers/productController");
 
 const router = express.Router();
 router.get("/:location", showNearByProducts);
+router.get("/recommendations/:userId", getRecommendedProducts);
 
 module.exports = router;
